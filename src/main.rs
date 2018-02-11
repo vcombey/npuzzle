c418335330ebe09c916ed113a5c2254ed1fab5a6
vcombey <vcombey@student.42.fr>
2018-02-11 22:39:50 +0100

    parsing seems fine

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,30 @@
+extern crate npuzzle;
+use npuzzle::taquin::Taquin;
 use std::fs::File;
 use std::io::Read;
 use std::env;
-use std::num::ParseIntError;
-use std::str::FromStr;
-#[macro_use] extern crate failure;
 
-use failure::Error;
-
-#[derive(Debug, Fail, PartialEq)]
-enum ParseNpuzzleError {
-    #[fail(display = "invalid dimension")]
-    BadDim,
-}
-
-fn read_file(filename: &str) -> Result<String, Error> {
+fn read_file(filename: &str) -> Result<String, std::io::Error> {
     let mut f = File::open(filename)?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
     Ok(s)
 }
 
-#[derive(Debug, PartialEq)]
-struct Npuzzle {
-    n: u64, 
-    pieces: Vec<u64>,
-}
-
-impl FromStr for Npuzzle {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-
-        // remove comments
-        let mut lines = s.lines().map(|l| {
-            l.get(..l.find('#').unwrap_or(l.len()))
-                .unwrap().trim() // can't fail
-        }).filter(|l| l != &"");
-
-        // get dimension
-        let n: u64 = lines.next().ok_or(ParseNpuzzleError::BadDim)?.trim().parse()?;
-
-        // collect pieces
-        let pieces = lines.flat_map(|l| {
-             l.split_whitespace().map(|num| num.trim().parse())
-        }).collect::<Result<Vec<u64>, ParseIntError>>()?;
-
-        Ok(Npuzzle {
-            n,
-            pieces,
-        })
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn comments() {
-        let s = "# This puzzle is solvable
-3 #lol
-5 1 0 #1 2 3
-8 4 6            #
-3 7 2";
-        assert_eq!(s.parse::<Npuzzle>().unwrap(), Npuzzle { n: 3, pieces: vec![5, 1, 0, 8, 4, 6, 3, 7, 2] });
-    }
-    #[test]
-    fn tabulations() {
-        let s = "# This puzzle is solvable
-3#
- 5 1 0 #1 2 3
-8 4 6            #
-3 7 2";
-        assert_eq!(s.parse::<Npuzzle>().unwrap(), Npuzzle { n: 3, pieces: vec![5, 1, 0, 8, 4, 6, 3, 7, 2] });
-    }
-}
-
 fn main() {
-
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 { 
+    if args.len() != 2 {
         eprintln!("bad number of args, expected one");
-        return ;
+        return;
     }
     let s = match read_file(&args[1]) {
         Ok(s) => s,
-        Err(e) => {eprintln!("{}", e); return ;},
+        Err(e) => {
+            eprintln!("{}", e);
+            return;
+        }
     };
     println!("{}", s);
-    let npuzzle = s.parse::<Npuzzle>().unwrap();
-    println!("{:?}", npuzzle);
+    let taquin = s.parse::<Taquin>().unwrap();
+    println!("{:?}", taquin);
 }
---
+++ src/taquin.rs
@@ -0,0 +1,143 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Taquin {
+    n: usize,
+    pieces: Vec<u64>,
+}
+
+impl FromStr for Taquin {
+    type Err = ParseTaquinError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // remove comments
+        let mut lines = s.lines()
+            .map(|l| {
+                l.get(..l.find('#').unwrap_or_else(|| l.len()))
+                    .unwrap()
+                    .trim() // can't fail
+            })
+            .filter(|l| l != &"");
+
+        // get dimension
+        let n: usize = lines.next().ok_or(ParseTaquinError::Empty)?.trim().parse()?;
+
+        // collect pieces
+        let pieces = lines
+            .map(|l| l.split_whitespace().map(|num| num.trim().parse()).collect())
+            .collect::<Result<Vec<Vec<u64>>, ParseIntError>>()?;
+
+        if pieces.len() != n {
+            return Err(ParseTaquinError::BadNbLine);
+        }
+
+        if !pieces.iter().all(|v| v.len() == n) {
+            return Err(ParseTaquinError::BadNbColonne);
+        }
+
+        let pieces = pieces.into_iter().flat_map(|l| l).collect::<Vec<u64>>();
+
+        Ok(Taquin { n, pieces })
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ParseTaquinError {
+    Empty,
+    BadNbColonne,
+    BadNbLine,
+    BadNoTakin(ParseIntError),
+}
+
+impl fmt::Display for ParseTaquinError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "coucou")
+    }
+}
+
+impl Error for ParseTaquinError {
+    fn description(&self) -> &str {
+        match *self {
+            ParseTaquinError::Empty => "the taquin is empty",
+            ParseTaquinError::BadNbColonne => "bad number of colonne",
+            ParseTaquinError::BadNbLine => "bad number of line",
+            ParseTaquinError::BadNoTakin(ref e) => e.description(),
+        }
+    }
+}
+
+impl From<ParseIntError> for ParseTaquinError {
+    fn from(error: ParseIntError) -> Self {
+        ParseTaquinError::BadNoTakin(error)
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn empty() {
+        let s = "# This puzzle is solvable
+
+
+";
+        assert_eq!(s.parse::<Taquin>(), Err(ParseTaquinError::Empty));
+    }
+    #[test]
+    fn comments() {
+        let s = "# This puzzle is solvable
+3 #lol
+5 1 0 #1 2 3
+8 4 6            #
+3 7 2";
+        assert_eq!(
+            s.parse::<Taquin>().unwrap(),
+            Taquin {
+                n: 3,
+                pieces: vec![5, 1, 0, 8, 4, 6, 3, 7, 2],
+            }
+        );
+    }
+    #[test]
+    fn tabulations() {
+        let s = "# This puzzle is solvable
+                   3#
+                   5 1 0 #1 2 3
+                   8 4 6            #
+                   3 7 2";
+        assert_eq!(
+            s.parse::<Taquin>().unwrap(),
+            Taquin {
+                n: 3,
+                pieces: vec![5, 1, 0, 8, 4, 6, 3, 7, 2],
+            }
+        );
+    }
+    #[test]
+    fn bad_integer() {
+        let s = " 3
+                   a 1 0
+                   8 4 6
+                   3 7 2";
+        assert!(s.parse::<Taquin>().is_err());
+        //assert_eq!(s.parse::<Taquin>(), Err(ParseTaquinError::BadNoTakin(_)));
+    }
+    #[test]
+    fn bad_nb_colonne() {
+        let s = "3
+                   1 1 0 1
+                   8 4 6
+                   3 7 2";
+        assert_eq!(s.parse::<Taquin>(), Err(ParseTaquinError::BadNbColonne));
+    }
+    #[test]
+    fn bad_nb_line() {
+        let s = "3
+                   1 1 0
+                   8 4 6
+                   8 4 6
+                   3 7 2";
+        assert_eq!(s.parse::<Taquin>(), Err(ParseTaquinError::BadNbLine));
+    }
+}
