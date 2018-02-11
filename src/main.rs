69545cdf5224e292df69855a2636b8a84f3ab628
vcombey <vcombey@student.42.fr>
2018-02-11 14:12:51 +0100

    file read to string

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+use std::io::Error;
+use std::io::Read;
+use std::env;
+
+fn read_file(filename: &str) -> Result<String, Error> {
+    let mut f = File::open(filename)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 { 
+        eprintln!("bad number of args, expected one");
+        return ;
+    }
+    let s = match read_file(&args[1]) {
+        Ok(s) => s,
+        Err(e) => {eprintln!("{}", e); return ;},
+    };
+    println!("{}", s);
+}
