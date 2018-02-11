2dfe9e2eec96c1b2512808a244d59d8e4dbaddfb
Vincent COMBEY <vcombey@student.42.fr>
2018-02-11 17:07:37 +0100

    start parsing npuzzle, doe'nt work with tabs and nb of numbers per line is unchecked

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use std::fs::File;
-use std::io::Error;
 use std::io::Read;
 use std::env;
+use std::num::ParseIntError;
+use std::str::FromStr;
+#[macro_use] extern crate failure;
+
+use failure::Error;
+
+// This is a new error type that you've created. It represents the ways a
+// toolchain could be invalid.
+//
+// The custom derive for Fail derives an impl of both Fail and Display.
+// We don't do any other magic like creating new types.
+
+#[derive(Debug, Fail, PartialEq)]
+enum ParseNpuzzleError {
+    #[fail(display = "invalid dimension")]
+    BadDim,
+}
 
 fn read_file(filename: &str) -> Result<String, Error> {
     let mut f = File::open(filename)?;
@@ -10,8 +26,62 @@ fn read_file(filename: &str) -> Result<String, Error> {
     Ok(s)
 }
 
+#[derive(Debug, PartialEq)]
+struct Npuzzle {
+    n: u64, 
+    pieces: Vec<u64>,
+}
+
+impl FromStr for Npuzzle {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+
+        // remove comments
+        let mut lines = s.lines().map(|l| {
+            l.get(..l.trim().find('#').unwrap_or(l.len()))
+                .unwrap() // can't fail
+        }).filter(|l| l != &"");
+
+        // get dimension
+        let n: u64 = lines.next().ok_or(ParseNpuzzleError::BadDim)?.trim().parse()?;
+
+        // collect pieces
+        let pieces = lines.flat_map(|l| {
+             l.split_whitespace().map(|num| num.trim().parse())
+        }).collect::<Result<Vec<u64>, ParseIntError>>()?;
+
+        Ok(Npuzzle {
+            n,
+            pieces,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn comments() {
+        let s = "# This puzzle is solvable
+3 #lol
+5 1 0 #1 2 3
+8 4 6            #
+3 7 2";
+        assert_eq!(s.parse::<Npuzzle>().unwrap(), Npuzzle { n: 3, pieces: vec![5, 1, 0, 8, 4, 6, 3, 7, 2] });
+    }
+    #[test]
+    fn tabulations() {
+        let s = "# This puzzle is solvable
+    3 #lol
+5 1 0 #1 2 3
+8 4 6            #
+3 7 2";
+        assert_eq!(s.parse::<Npuzzle>().unwrap(), Npuzzle { n: 3, pieces: vec![5, 1, 0, 8, 4, 6, 3, 7, 2] });
+    }
+}
+
 fn main() {
-    
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 { 
         eprintln!("bad number of args, expected one");
@@ -22,4 +92,6 @@ fn main() {
         Err(e) => {eprintln!("{}", e); return ;},
     };
     println!("{}", s);
+    let npuzzle = s.parse::<Npuzzle>().unwrap();
+    println!("{:?}", npuzzle);
 }
