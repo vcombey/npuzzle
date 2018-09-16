0763a9203e5f26f6479a204070b4220cba7bda6b
Sebastien CLOLUS <sclolus@e1r7p4.42.fr>
2018-09-16 16:27:49 +0200

    removed some deadcode

--- src/construct_pruning_trie.rs
+++ src/construct_pruning_trie.rs
@@ -1,6 +1,5 @@
-use std::cmp::Ordering;
-//use std::collections::BinaryHeap;
 use maxdir::MaxDir;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use taquin::{Dir, Taquin};
@@ -93,94 +92,6 @@ pub fn construct_pruning_trie(
             }
         }
     }
-
-    /*let mut v: Vec<Vec<Node>> = closed_set.into_iter().map(|(t, c)| c).collect();
-    v.sort_by_key(|b| b[0].path.len());
-    for mut e in v {
-    let primitive = e.remove(0);
-    println!("\nprimitive {:?}", primitive.path);
-
-    for redundant in e.into_iter() {
-    println!("redundant {:?}", redundant.path);
-    nb_duplicate += 1;
-    redundant_paths.push(redundant.path);
-}
-    primitive_paths.push(primitive.path);
-}
-    redundant_paths.sort_by_key(|b| b.len());
-    for r in &redundant_paths {
-    trie.add_word(r);
-    debug_assert!(trie.match_word(r.iter()) == TrieType::Redundant);
-}
-    println!("nb duplicate {}", nb_duplicate);
-     */
     trie.update_failure();
     (trie, redundant_paths, primitive_paths)
 }
-/*
-pub fn construct_pruning_trie() -> (Trie, Vec<Vec<Dir>>, Vec<Vec<Dir>>) {
-let spiral = Taquin::spiral(7);
-let mut closed_set: HashMap<Taquin, BinaryHeap<Node>> =
-HashMap::with_capacity(DEFAULT_CLOSED_SET_SIZE);
-let mut open_set = VecDeque::with_capacity(DEFAULT_OPEN_SET_SIZE);
-let init_node = Node::new(Vec::new(), spiral.clone(), MaxDir::new([0; 4], 0, 0));
-let mut trie = Trie::new();
-let mut primitive_paths = Vec::new();
-let mut redundant_paths = Vec::new();
-
-open_set.push_back(init_node.clone());
-closed_set.insert(spiral, vec![init_node].into());
-let mut nb_duplicate = 0;
-while let Some(curr) = open_set.pop_front() {
-if curr.path.len() > MAX_DEPTH - 1 {
-break;
-        }
-        for d in [Dir::Right, Dir::Up, Dir::Down, Dir::Left].into_iter() {
-            if let Some(neighbour) = curr.taquin.move_piece(*d) {
-                let neighbour_node = Node::from_curr(&curr, *d, neighbour.clone());
-                if closed_set.contains_key(&neighbour) {
-                    closed_set.get_mut(&neighbour).unwrap().push(neighbour_node);
-                } else {
-                    open_set.push_back(neighbour_node.clone());
-                    closed_set.insert(neighbour, vec![neighbour_node].into());
-                }
-                /*if closed_set.contains_key(&neighbour) {
-                    //println!("{:?}", neighbour_path);
-                    if trie.add_word(&neighbour_node.path) {
-                        nb_duplicate += 1;
-                    }
-                }
-                else {
-                    all_redundant_paths.push(neighbour_node.path.clone());
-                    //println!("tree: {:#?}", trie);
-                    primitive_paths.push(neighbour_node.path.clone());
-                    open_set.push_back(neighbour_node);
-                    closed_set.insert(neighbour);
-                }*/
-            }
-        }
-    }
-    let mut v: Vec<BinaryHeap<Node>> = closed_set.into_iter().map(|(t, c)| c).collect();
-    v.sort_unstable_by_key(|b| b.peek().unwrap().path.len());
-    for mut e in v {
-        let primitive = e.pop().unwrap();
-        println!("\nprimitive {:?}", primitive.path);
-        
-        for redundant in e {
-            println!("redundant {:?}", redundant.path);
-            //if redundant.partial_cmp(&primitive).is_some() {
-                nb_duplicate += 1;
-                trie.add_word(&redundant.path);
-                redundant_paths.push(redundant.path);
-            //} else if trie.match_word(redundant.path.iter()) != TrieType::Redundant {
-            //    primitive_paths.push(redundant.path);
-            //}
-        }
-        //if trie.match_word(primitive.path.iter()) != TrieType::Redundant {
-            primitive_paths.push(primitive.path);
-        //}
-    }
-    println!("nb duplicate {}", nb_duplicate);
-    (trie, redundant_paths, primitive_paths)
-}
-*/
